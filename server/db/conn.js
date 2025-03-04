b083037517cf8f44be3e680a069804d8dabbdc7d
ravi shekhar <ravishekhar1095@gmail.com>
2025-03-04 16:30:27 +0530

    added my db credentials

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,6 +1,6 @@
 // const mongoose = require('mongoose');
 
-// const DB = "mongodb+srv://ravishekharvfirst:Anaconda@mailspace.odp28.mongodb.net/"; // 
+const DB = "mongodb+srv://ravishekharvfirst:Anaconda@mailspace.odp28.mongodb.net/?retryWrites=true&w=majority&appName=mailspace"; // 
 
 // mongoose.connect(DB)
 //     .then(() => console.log("Connected to MongoDB Atlas"))
@@ -10,7 +10,7 @@
 
     const mongoose = require('mongoose');
     // const DB ="mongodb+srv://rajputrashmi321:AETaXiLuLI2UoNjk@clustertest.cwyrb.mongodb.net/spaceDB?retryWrites=true&w=majority&appName=ClusterTest"
-const DB ="mongodb+srv://anjalithakur96505:vqrkxZHioMMtQoFk@cluster2.mqa6q.mongodb.net/authuser?retryWrites=true&w=majority&appName=Cluster2"
+//const DB ="mongodb+srv://anjalithakur96505:vqrkxZHioMMtQoFk@cluster2.mqa6q.mongodb.net/authuser?retryWrites=true&w=majority&appName=Cluster2"
 mongoose.connect(DB,{
     useUnifiedTopology: true,
     useNewUrlParser: true
